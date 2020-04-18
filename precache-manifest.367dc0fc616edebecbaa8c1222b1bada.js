de8d01adbe5171a73bd1b08e608952804d9e1d03
TossACoinToUrProgrammer <arlen.manasov@icloud.com>
2020-04-18 21:20:52 +0600

    Updates

---
+++ precache-manifest.367dc0fc616edebecbaa8c1222b1bada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22e50aa3d30bfe1852d6f1ded995ca1",
+    "url": "/contacts-app/index.html"
+  },
+  {
+    "revision": "a8442043911974534332",
+    "url": "/contacts-app/static/css/main.cc55934d.chunk.css"
+  },
+  {
+    "revision": "1541ff37e303dd7ef916",
+    "url": "/contacts-app/static/js/2.2bd2c3fc.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/contacts-app/static/js/2.2bd2c3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8442043911974534332",
+    "url": "/contacts-app/static/js/main.baa78273.chunk.js"
+  },
+  {
+    "revision": "46978999a4279c631d22",
+    "url": "/contacts-app/static/js/runtime-main.bcae1327.js"
+  },
+  {
+    "revision": "c3349addcff3d10fb3284f6882d2238e",
+    "url": "/contacts-app/static/media/loader4.c3349add.gif"
+  }
+]);
